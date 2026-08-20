cee52705f1e63357f83775573fb7636b8fc73074
Sebastian Urciuolo <101946505+sebasu66@users.noreply.github.com>
2026-08-19 23:05:11 -0300

    ui: switch prototype to spatial spread workspace

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
-import './styles.css';
+import SpatialApp from './SpatialApp';
+import './spatial.css';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(<React.StrictMode><App /></React.StrictMode>);
+ReactDOM.createRoot(document.getElementById('root')!).render(<React.StrictMode><SpatialApp /></React.StrictMode>);
